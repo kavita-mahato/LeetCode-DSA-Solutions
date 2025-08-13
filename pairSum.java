113abfa980791f7ff25dc9fb5cf927e411a26025
Kavita Mahato <kavitamahato0835@gmail.com>
2025-08-13 11:44:51 +0530

    Two Sum II - Input Array is Sorted

---
+++ pairSum.java
@@ -0,0 +1,18 @@
+public class pairSum {
+    public int[] twoSum(int[] numbers, int target) { 
+        int n = numbers.length;
+        int left=0, right=n-1;
+        int[] pair = {-1, -1};
+        while(left < right){
+            int pairSum = numbers[left] + numbers[right];
+            if(pairSum == target){
+                pair[0] = left + 1;
+                pair[1] = right + 1;
+                return pair;
+            }
+            else if(pairSum > target) right--;
+            else left++;
+        }
+        return pair;
+    }
+}
