47c6289055b740cb262ee6735e58917a42dde294
Kavita Mahato <kavitamahato0835@gmail.com>
2025-08-14 20:41:24 +0530

    Plus One

---
+++ S05_Arrays/onePlus.java
@@ -0,0 +1,20 @@
+package S05_Arrays;
+
+public class onePlus {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        
+        for (int i = n-1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits; // no carry, done
+            }
+            digits[i] = 0; // set to 0 and carry over
+        }
+        
+        // If we reached here, all digits were 9
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+}
