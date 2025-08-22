e7400a0f41b76d0afc1e8e1f00563546bf9c3208
Kavita Mahato <kavitamahato0835@gmail.com>
2025-08-23 00:23:04 +0530

    Merge Sorted Array

---
+++ S07_Sorting/mergeSorted.java
@@ -0,0 +1,49 @@
+public class mergeSorted {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        // Sort left half
+        mergeSort(arr, left, mid);
+
+        // Sort right half
+        mergeSort(arr, mid + 1, right);
+
+        // Merge two halves
+        merge(arr, left, mid, right);
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        // Sizes of subarrays
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data
+        for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+        // Merge process
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        // Copy remaining elements
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+}
